22ca0dec894b84fff80d002130255d25beae2aa8
Lucas Gabriel <lucas.alves@geracaoti.net>
2019-12-19 10:26:55 -0300

    Progresso noBlog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,32 +1,55 @@
 class ArticlesController < ApplicationController
 
-    def index
-        @articles = Article.all
+  def index
+    @articles = Article.all
+  end
+
+  def new 
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end 
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    # cannot render two times
+    #render plain: params[:arcticle].inspect
+
+    #ActiveModel::ForbiddenAttributesError
+    #@article = Article.new(params[:article])
+
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else 
+      render 'new'
     end
+  end
 
-    def new
+  def update 
+    @article = Article.find(params[:id])
+    if(@article.update(article_params))
+      redirect_to @article
+    else 
+      render 'edit'
     end
+  end
 
-    def show
-        @article = Article.find(params[:id])
-    end
+  def destroy 
+    @article = Article.find(params[:id])
+    @article.destroy
 
-    def create
-        #cannot render two times
-        #render plain: params[:arcticle].inspect
+    redirect_to articles_path
+  end
 
-        #ActiveModel::ForbiddenAttributesError
-        #@article = Article.new(params[:article])
-
-        @article = Article.new(article_params)
-
-        @article.save
-        redirect_to @article
-    end    
-
-    private 
-    def article_params
-        params.require(:article).permit(:title, :text)
-    end 
+  private 
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 
 end
